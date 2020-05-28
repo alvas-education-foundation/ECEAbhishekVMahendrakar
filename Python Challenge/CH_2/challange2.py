26e83b7e6b66090fd4fc15af816672091f367a61
AbhishekVasudevMahendrakar <abhi2244mahendrakar@gmail.com>
2020-05-28 22:34:59 +0530

    Created folders for code challenge

---
+++ Python Challenge/CH_2/challange2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 19:36:14 2020
+
+Name: Abhishek Vasudev Mahendrakar
+USN: 4AL17EC003
+Student at Alva's Institute of Engineering and Technology.
+E-mail: abhi2244mahendrakar@gmail.com
+Github: AbhishekVasudevMahendrakar 
+"""
