f1226883e47131bbb6eff8e56fec14c9172f9596
AbhishekVasudevMahendrakar <abhi2244mahendrakar@gmail.com>
2020-05-31 22:18:04 +0530

    one

--- Python Challenge/CH_2/challange2.py
+++ Python Challenge/CH_2/challange2.py
@@ -8,3 +8,28 @@ Student at Alva's Institute of Engineering and Technology.
 E-mail: abhi2244mahendrakar@gmail.com
 Github: AbhishekVasudevMahendrakar 
 """
+import pandas as pd
+
+import matplotlib.pyplot as plt
+import csv
+
+x=[]
+y=[]
+
+with open('a01_3.csv', 'r') as csvfile:
+    plots= csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        x.append((row[0]))
+        y.append((row[1]))
+
+
+plt.plot(x,y, marker='o')
+
+plt.title('ECG Signal')
+
+plt.xlabel('Time')
+plt.ylabel('Raw_ECG')
+plt.xlim(500,520)
+plt.ylim(20,400)
+
+plt.show()
